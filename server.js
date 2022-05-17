557fe19e697a5acff1d6be8e40a6987f3207d7e4
selaguardia <selaguardia91@gmail.com>
2022-05-17 10:27:34 -0700

    adds user files for controllers, models. adds auth file for config. Installs dependencies. adds jwt token

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+cons
 
 const PORT = 3000;
 
@@ -7,6 +9,9 @@ app.use((req, res, next) => {
   console.log(`METHOD:${req.method} \nOG-URL:${req.originalUrl}`);
   next();
 });
+
+app.use(require('./config/auth'));
+app.use('/users', require('./routes/users'));
 
 app.get("/", (req, res) => {
   res.send('Team 1 Homepage')
